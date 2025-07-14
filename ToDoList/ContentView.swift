e5feb194781e6cfdc69dc8a40083fb472d55c689
Nicole Bond <nicbond22@gmail.com>
2025-07-14 10:25:48 -0400

    Plus sign shows new view on same page, and it also gradually appears

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    
     var body: some View {
+           
         VStack {
             HStack{
                 Text("To-Do List")
@@ -18,7 +21,9 @@ struct ContentView: View {
                 Spacer()
                 
                 Button{
-                    
+                    withAnimation {
+                        showNewTask = true
+                    }
                 } label: {
                     Text("+")
                         .font(.title)
@@ -29,6 +34,9 @@ struct ContentView: View {
         .padding()
         Spacer()
         
+        if showNewTask {
+            NewToDoView()
+        }
         
     }
 }
